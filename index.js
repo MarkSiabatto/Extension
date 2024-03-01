5277b5439fa1e45255f77645d13eee852be14b53
Siabatto Rodriguez <siabattorodriguezmarco@gmail.com>
2024-03-01 13:13:42 -0500

    Popup created, uploading with async js file.

---
+++ index.js
@@ -0,0 +1,10 @@
+async function sayHello() {
+    let [tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+            alert("This is the extension Alert!!")
+        }
+    })
+}
+document.getElementById('myButton').addEventListener("click", sayHello)
